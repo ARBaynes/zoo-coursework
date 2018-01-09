cff66c82f6aa365a0d67d6ec7bc29ac8dfb03847
Annabel <annabel.baynes@gmail.com>
2018-01-09 14:58:47 +0000

    Added contents of Staff class

--- src/main/classes/staff/Staff.java
+++ src/main/classes/staff/Staff.java
@@ -1,4 +1,41 @@
 package main.classes.staff;
 
-public class Staff {
+import java.io.Serializable;
+
+public class Staff implements Serializable{
+    private String name;
+    private String preferredPenType = "";
+    private Integer staffID;
+
+    public Staff (String name) {
+        setName(name);
+    }
+
+    //SETTERS
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPreferredPenType(String preferredPenType) {
+        this.preferredPenType = preferredPenType;
+    }
+
+    public void setStaffID(Integer staffID) {
+        this.staffID = staffID;
+    }
+
+    //GETTERS
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPreferredPenType() {
+        return preferredPenType;
+    }
+
+    public Integer getStaffID() {
+        return staffID;
+    }
 }
