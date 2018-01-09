86c6f2a6602645977f1cf8fc70ea4e963611adbf
Annabel <annabel.baynes@gmail.com>
2018-01-09 16:34:20 +0000

    Added outline of DryModel methods

--- src/main/modules/pens/dry/DryModel.java
+++ src/main/modules/pens/dry/DryModel.java
@@ -1,4 +1,52 @@
 package main.modules.pens.dry;
 
+import main.classes.pens.Aquarium;
+import main.classes.pens.Dry;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class DryModel {
+
+    protected static String filePath = "data/pen_data/dry_data/";
+    protected static ArrayList<Dry> allDryPens = new ArrayList<>();
+
+    //SETTERS
+
+    public static void setAllDryPens() {
+    }
+
+
+    //GETTERS
+
+    public static ArrayList<Dry> getAllDryPens() {
+        return allDryPens;
+    }
+
+
+    //PUBLIC DATA MANIPULATION
+
+    public static void addDryPen (Dry dry) {
+
+    }
+
+    public static void editDryPen (Dry toFind, Dry toReplaceWith) {
+
+    }
+
+    public static void removeDryPen (Dry toFind) {
+
+    }
+
+    //PRIVATE DATA MANIPULATION
+
+    private static void serialize (Dry aquarium) {
+
+    }
+
+    private static Dry deserialize (File toRead) {
+        return null;
+    }
+
+    //GENERAL PURPOSE
 }
