a8dd2a776ab38de17decd678f169b3746903b349
Annabel <annabel.baynes@gmail.com>
2018-01-09 14:02:50 +0000

    Added Dry pen class

--- src/main/classes/pens/Dry.java
+++ src/main/classes/pens/Dry.java
@@ -1,4 +1,14 @@
 package main.classes.pens;
 
-public class Dry extends Pen {
+import java.io.Serializable;
+
+public class Dry extends Pen implements Serializable {
+    public Dry () {
+        setPenType();
+    }
+
+    @Override
+    public void setPenType() {
+        penType = "dry";
+    }
 }
