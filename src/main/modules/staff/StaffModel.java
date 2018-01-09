c0fb818f5e50865ff056f4bff9daff60ddbb2312
Annabel <annabel.baynes@gmail.com>
2018-01-09 16:17:02 +0000

    Added outline of StaffModel methods

--- src/main/modules/staff/StaffModel.java
+++ src/main/modules/staff/StaffModel.java
@@ -1,4 +1,51 @@
 package main.modules.staff;
 
+import main.classes.staff.Staff;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class StaffModel {
+    protected static String filePath = "data/staff_data/";
+    protected static ArrayList<Staff> allStaff = new ArrayList<>();
+
+    //SETTERS
+
+    public static void setAllStaff() {
+    }
+
+
+    //GETTERS
+
+    public static ArrayList<Staff> getAllStaff() {
+        return allStaff;
+    }
+
+
+    //PUBLIC DATA MANIPULATION
+
+    public static void addStaff (Staff staff) {
+
+    }
+
+    public static void editStaff (Staff toFind, Staff toReplaceWith) {
+
+    }
+
+    public static void removeStaff (Staff toFind) {
+
+    }
+
+    //PRIVATE DATA MANIPULATION
+
+    private static void serialize (Staff staff) {
+
+    }
+
+    private static Staff deserialize (File toRead) {
+        return null;
+    }
+
+    //GENERAL PURPOSE
+
 }
