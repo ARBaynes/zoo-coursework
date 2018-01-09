7ec624e6c67919fd5465b7326f5b0a7ad4f44e1e
Annabel <annabel.baynes@gmail.com>
2018-01-09 14:00:29 +0000

    Added Aquarium class

--- src/main/classes/pens/Aquarium.java
+++ src/main/classes/pens/Aquarium.java
@@ -2,5 +2,35 @@ package main.classes.pens;
 
 import main.interfaces.pens.*;
 
-public class Aquarium extends Pen implements HighPen, WaterPen{
+import java.io.Serializable;
+
+public class Aquarium extends Pen implements HighPen, WaterPen, Serializable{
+    private Integer height;
+    private Integer waterVolume;
+    private String waterType;
+
+    public Aquarium () {
+        setPenType();
+    }
+
+    @Override
+    public void setPenType() { penType = "aquarium"; }
+
+    @Override
+    public void setHeight(Integer height) { this.height = height; }
+
+    @Override
+    public int getHeight() { return height; }
+
+    @Override
+    public void setWaterVolume(Integer waterVolume) { this.waterVolume = waterVolume; }
+
+    @Override
+    public Integer getWaterVolume() { return waterVolume; }
+
+    @Override
+    public void setWaterType(String waterType) { this.waterType = waterType;    }
+
+    @Override
+    public String getWaterType() { return waterType; }
 }
