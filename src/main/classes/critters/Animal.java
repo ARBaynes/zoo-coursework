0d5c47c8f3c9b0af41fc81dce26a8dd4744604f2
Annabel <annabel.baynes@gmail.com>
2018-01-09 13:49:33 +0000

    Added Animal and Breed classes

--- src/main/classes/critters/Animal.java
+++ src/main/classes/critters/Animal.java
@@ -1,4 +1,57 @@
 package main.classes.critters;
 
-public class Animal {
+import java.io.Serializable;
+
+public class Animal implements Serializable{
+    private String name;
+    private Breed breed;
+    private Integer ID;
+    private boolean hasPen;
+
+    public Animal (String name, Breed breed) {
+        this.name = name;
+        this.breed = breed;
+        setHasPen(false);
+    }
+
+    //SETTERS
+
+    public void setHasPen(boolean hasPen) {
+        this.hasPen = hasPen;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    //GETTERS
+
+    public Integer getID() { return ID; }
+
+    public String getName() { return name; }
+
+    public Breed getBreed() { return breed; }
+
+    public boolean hasPen() { return hasPen; }
+
+    //BREED GETTERS
+
+    public String getBreedName () {return breed.getName();}
+    public String getBreedPenType  () {return breed.getPenType();}
+    public Integer getBreedRequirements () {return breed.getRequirements();}
+
+    //OTHER
+
+    @Override
+    public String toString() {
+        return  getID() + " - " + getName();
+    }
 }
--- src/main/classes/critters/Breed.java
+++ src/main/classes/critters/Breed.java
@@ -1,4 +1,49 @@
 package main.classes.critters;
 
-public class Breed {
+import java.io.Serializable;
+
+public class Breed implements Serializable {
+    private String name;
+    private String penType;
+    private Integer requirements;
+
+    public Breed (String name, String penType, Integer requirements) {
+        this.setName(name);
+        this.setPenType(penType);
+        this.setRequirements(requirements);
+    }
+
+    //SETTERS
+
+    public void setPenType(String penType) {
+        this.penType = penType;
+    }
+
+    public void setRequirements(Integer requirements) {
+        this.requirements = requirements;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    //GETTERS
+
+    public String getPenType() {
+        return penType;
+    }
+
+    public Integer getRequirements() {
+        return requirements;
+    }
+
+    public String getName() { return name; }
+
+    //OTHER
+
+    @Override
+    public String toString() {
+        return  "Breed Name: " + getName() + System.lineSeparator() +
+                "Pen Type: " + getPenType() + System.lineSeparator() +
+                "Area Requirements:  " + getRequirements() + System.lineSeparator();
+
+    }
 }
