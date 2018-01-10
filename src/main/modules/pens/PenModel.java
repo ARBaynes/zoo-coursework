2c754859ae59affc6f98e33a5cf764f7b18d6b1a
Annabel <annabel.baynes@gmail.com>
2018-01-10 14:04:11 +0000

    Developed the majority of the AquariumModel class

---
+++ src/main/modules/pens/PenModel.java
@@ -0,0 +1,24 @@
+package main.modules.pens;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.File;
+
+public abstract class PenModel {
+    protected static Integer createID (String filePath) {
+        Integer ID = 0;
+        File folder = new File(filePath);
+        if (folder.exists()) {
+            ID = folder.listFiles().length + 1;
+        }
+        return ID;
+    }
+
+
+    public static ObservableList<String> getAllPenTypes () {
+        return FXCollections.observableArrayList(
+                "Aquarium","Aviary","Dry","Part Water, Part Dry","Petting"
+        );
+    }
+}
--- src/main/modules/pens/aquarium/AquariumModel.java
+++ src/main/modules/pens/aquarium/AquariumModel.java
@@ -1,12 +1,16 @@
 package main.modules.pens.aquarium;
 
 import main.classes.pens.Aquarium;
+import main.classes.pens.Pen;
 import main.classes.staff.Staff;
+import main.modules.pens.PenModel;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class AquariumModel {
+public class AquariumModel extends PenModel{
     protected static String filePath = "data/pen_data/aquarium_data/";
     protected static ArrayList<Aquarium> allAquariums = new ArrayList<>();
 
@@ -32,26 +36,61 @@ public class AquariumModel {
 
     //PUBLIC DATA MANIPULATION
 
-    public static void addAquarium (Aquarium aquarium) {
-
+    public static void addAquarium (Aquarium pen) {
+        serialize(pen);
+        setAllAquariums();
     }
 
     public static void editAquarium (Aquarium toFind, Aquarium toReplaceWith) {
-
+        removeAquarium(toFind);
+        addAquarium(toReplaceWith);
     }
 
     public static void removeAquarium (Aquarium toFind) {
-
+        try {
+            Path pathToFile = Paths.get(filePath + toFind.getPenID() + ".aquarium");
+            java.nio.file.Files.deleteIfExists(pathToFile);
+            System.out.println("File Deleted.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setAllAquariums();
     }
 
     //PRIVATE DATA MANIPULATION
 
-    private static void serialize (Aquarium aquarium) {
-
+    private static void serialize (Aquarium pen) {
+        if (pen.getPenID() == null) { pen.setPenID(createID(filePath)); }
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream(filePath + pen.getPenID() +".aquarium");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(pen);
+            out.close();
+            fileOut.close();
+            System.out.printf("Serialized aquarium pen data is saved!!");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
     }
 
     private static Aquarium deserialize (File toRead) {
-        return null;
+        Aquarium pen = null;
+        try {
+            FileInputStream fileIn = new FileInputStream(toRead);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            pen = (Aquarium) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Aquarium class not found");
+            c.printStackTrace();
+            return null;
+        }
+        return pen;
     }
 
     //GENERAL PURPOSE
